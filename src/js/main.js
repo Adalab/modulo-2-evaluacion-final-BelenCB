0bddc0c9b1c1142ba8de383e3f106af86fc495a2
BelenCB <belencolladobarrena@gmail.com>
2025-09-06 14:22:56 +0200

    creado buscador de productos por nombre

--- src/js/main.js
+++ src/js/main.js
@@ -2,38 +2,66 @@
 
 const popularProducts = document.querySelector(".popular-products");
 
+let allProducts = [];
+
 const getProductsInfo = () => {
     fetch('https://fakestoreapi.com/products')
     .then((response) => {
         return response.json()
         })
     .then((data) => {
-        console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            const newProduct = document.createElement('div');
-            newProduct.classList.add("new-product");
-            const newImage = document.createElement('img');
-            newImage.src = data[i].image;
-            newImage.classList.add("new-image");
-            const newTitle = document.createElement('p');
-            newTitle.textContent = data[i].title;
-            newTitle.classList.add("new-title");
-            const newPrice = document.createElement('p');
-            newPrice.textContent = data[i].price + "€";
-            newPrice.classList.add("new-price");
-            const buttonBuy = document.createElement('button');
-            buttonBuy.textContent = "Comprar";
-            buttonBuy.classList.add("button-buy");
-            newProduct.appendChild(newImage);
-            newProduct.appendChild(newTitle);
-            newProduct.appendChild(newPrice);
-            newProduct.appendChild(buttonBuy);
-            popularProducts.appendChild(newProduct);
-        };
+        allProducts = data;
+        console.log(allProducts);
+        renderProducts(allProducts);   
+    });
+};
+
+const renderProducts = (products) => {
+    popularProducts.innerHTML = "";
+    products.forEach((product) => {
+        const newProduct = document.createElement('div');
+        newProduct.classList.add("new-product");
+
+        const newImage = document.createElement('img');
+        newImage.src = product.image;
+        newImage.classList.add("new-image");
+
+        const newTitle = document.createElement('p');
+        newTitle.textContent = product.title;
+        newTitle.classList.add("new-title");
+
+        const newPrice = document.createElement('p');
+        newPrice.textContent = product.price + "€";
+        newPrice.classList.add("new-price");
+
+        const buttonBuy = document.createElement('button');
+        buttonBuy.textContent = "Comprar";
+        buttonBuy.classList.add("button-buy");
+
+        newProduct.appendChild(newImage);
+        newProduct.appendChild(newTitle);
+        newProduct.appendChild(newPrice);
+        newProduct.appendChild(buttonBuy);
+
+        popularProducts.appendChild(newProduct);
     });
 };
 
 getProductsInfo();
 
+// buscador para filtar productos por su nombre
+
+const searchInput = document.querySelector(".searcher--input");
+const searchButton = document.querySelector(".searcher--button");
+
+const handleClickSearch = () => {
+    const filteredProducts = allProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchInput.value.toLowerCase())
+    );
+    renderProducts(filteredProducts);
+    console.log(filteredProducts);
+};
+
+searchButton.addEventListener("click", handleClickSearch);
 
 
